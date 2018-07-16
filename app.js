b09affcf4e0b1ed522780414980a01ec5ce4d645
4tal <idan.4tal@gmail.com>
2018-07-16 20:26:30 +0300

    app.js\Add routes for the static html pages + req(path)

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 console.log("App Started");
+
+var express = require('express');
+var app = express();
+
+var path = require('path');
+ 
+app.use(express.static('html'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname +'/html/index.html'));
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(__dirname +'/html/login.html'));
+})
+
+app.get('/signup', function (req, res) {
+    res.sendFile(path.join(__dirname +'/html/signup.html'));
+})
+
+// app.get('/signup', function (req, res) {
+//     res.sendFile(path.join(__dirname +'/html/signup.html'));
+// })
+
+
+
+app.listen(3000)
